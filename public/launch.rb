92094aeff4d7680fac2e63d758d985c252427b09
Kevin Incorvia <kevin@incorvia.me>
2012-11-07 23:19:26 -0500

    updated script using callbacks

--- public/launch.rb
+++ public/launch.rb
@@ -20,34 +20,101 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 require 'open-uri'
+require 'pry'
 
-# <------------------------------[ Setup ]---------------------------->
+# <-----------------------[ RailRocket Class ]----------------------->
 
-@rocket = {}
-@rocket[:installers] = []
+class RailRocket
+  include ActiveSupport::Callbacks
 
-# <-----------------------------[ Gemfile ]--------------------------->
+  attr_accessor :generator, :rocket
 
-remove_file("Gemfile")
-create_file("Gemfile", open('http://railrocket.me/templates/Gemfile').read)
+  def initialize(generator)
+    @generator = generator
+    @rocket = {}
+    @rocket[:engines] = []
+    @rocket[:launchers] = []
+  end
 
-# <-----------------------------[ Ask User ]-------------------------->
+  def method_missing(method_name, *args, &block)
+    if generator.respond_to?(method_name)
+      generator.send(method_name, *args, &block)
+    else
+      super
+    end
+  end
 
-# Git
-if yes?("\nInitiate a new git repository? (y|n)\n")
-  @rocket[:installers] << :git
+  def launch!
+    rocket[:engines].each do |engine|
+      run_callbacks "#{engine}_launcher" do
+        launcher(engine)
+      end
+    end
+  end
+
+  def launcher(engine)
+    "Starting #{engine} engine! ......."
+  end
 end
 
-# <-------------------------------[ Git ]----------------------------->
+# <---------------------------[ Engine ]--------------------------->
 
-if @rocket[:installers].include?(:git)
-  git :init
+class RailRocket
+  module Engine
+    include ActiveSupport::Inflector
+
+    def self.extended(klass)
+      klass.define_singleton_method :extended do |base|
+        engine = klass.name.demodulize.downcase.to_sym
+        base.rocket[:engines] << engine
+        base.class.define_callbacks "#{engine}_launcher"
+        base.class.set_callback "#{engine}_launcher", :before, "#{engine}_preflight"
+        base.class.set_callback "#{engine}_launcher", :after, "#{engine}_takeoff"
+      end
+    end
+  end
 end
 
-# <------------------------------[ Rspec ]---------------------------->
+# <-----------------------------[ Git ]----------------------------->
 
+class RailRocket
+  module Git
+    extend RailRocket::Engine
 
+    def git_preflight
+      if yes?("\nInitiate a new git repository? (y|n)\n")
+        rocket[:launchers] << :git
+      end
+    end
 
+    def git_takeoff
+      git :init
+    end
+  end
+end
 
+# <----------------------------[ Gemfile ]--------------------------->
 
+class RailRocket
+  module Gemfile
+    extend RailRocket::Engine
 
+    def gemfile_preflight
+    end
+
+    def gemfile_takeoff
+      remove_file("Gemfile")
+      data = open('http://www.railrocket.me/templates/Gemfile').read
+      create_file("Gemfile", data)
+    end
+  end
+end
+
+# <------------------------[ Run RailRocket ]------------------------->
+
+rocket = RailRocket.new(self)
+
+rocket.extend(RailRocket::Git)
+rocket.extend(RailRocket::Gemfile)
+
+rocket.launch!
