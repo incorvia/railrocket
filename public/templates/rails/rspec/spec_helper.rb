f47e91d90b4b0ef8744868de93e2214bbdc2c905
Kevin Incorvia <kevin@incorvia.me>
2012-12-17 07:58:56 -0500

    add poltergeist to spec helper

--- public/templates/rails/rspec/spec_helper.rb
+++ public/templates/rails/rspec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/poltergeist'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -50,3 +51,5 @@ RSpec.configure do |config|
     Timecop.return
   end
 end
+
+Capybara.javascript_driver = :poltergeist
